a39e0d97d0533eda8247eb18b1d9f64e56042029
RobWHickman <robwhickman@gmail.com>
2020-08-17 10:27:19 +0100

    messing with spadl script and added type ids

---
+++ R/spadl_config.R
@@ -0,0 +1,15 @@
+#' Type ids for spadl actions
+#'
+#' @docType data
+#'
+#' @usage data(spadl_type_ids)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://github.com/ML-KULeuven/socceraction/blob/master/socceraction/spadl/config.py}{socceraction repo}
+#'
+#' @examples
+#' types <- data(spadl_type_ids)
+"spadl_type_ids"
