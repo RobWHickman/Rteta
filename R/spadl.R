eec93dfb6af5e84c940354658e52c000ec96b964
RobWHickman <robwhickman@gmail.com>
2020-08-17 10:27:19 +0100

    messing with spadl script and added type ids

--- R/spadl.R
+++ R/spadl.R
@@ -9,6 +9,105 @@
 #' @author Robert Hickman
 #' @export sb_convert_spadl
 
+sb_convert_spadl <- function(match_events) {
+  #some useful variables
+  home_team <- match_events$team.id[1]
+
+  #init the actions df
+  spadl_df <- data.frame(
+    game_id = match_events$match_id,
+    period_id = match_events$period,
+    time_seconds = as.numeric(lubridate::hms(match_events$timestamp)),
+    timestamp = match_events$timestamp,
+    team_id = match_events$team.id,
+    team_name = match_events$team.name,
+    player_id = match_events$player.id,
+    player_name = match_events$player.name
+  )
+
+  #get the locations of events on pitch
+  #convert to spadl coordinates (uses length 105 and height 68)
+  match_locations <- Rteta::sb_clean_locations(match_events)
+  match_locations <- cbind(
+    apply(match_locations[grepl("^location.*x$", names(match_locations))], 2, Rteta::spadl_dict, type = "location_x", provider = "statsbomb"),
+    apply(match_locations[grepl("^location.*y$", names(match_locations))], 2, Rteta::spadl_dict, type = "location_y", provider = "statsbomb"),
+    match_locations[grepl("^location.*z$", names(match_locations))]
+  )
+  match_locations <- apply(match_locations, 2, round, digits = 2)
+  colnames(match_locations) <- gsub("(^location\\.)([a-z]$)", "start_\\2", colnames(match_locations))
+  colnames(match_locations) <- gsub("(^location\\.end\\.)([a-z]$)", "end_\\2", colnames(match_locations))
+
+  #get the actions on the pitch
+  actions <- match_events$type.name
+  actions_extra <- match_events[grepl("\\.type\\.name", names(match_events))]
+  actions_extra <- dplyr::coalesce(!!!actions_extra)
+  actions <- Rteta::spadl_dict("action", "statsbomb", actions)
+
+  #mapply this later
+  #manual munging
+  actions[actions == "pass" & actions_extra == "Free Kick" & (match_events$pass.height.name == "High Pass" | match_events$pass.cross)] <- "freekick_crossed"
+  actions[actions == "pass" & actions_extra == "Free Kick"] <- "freekick_short"
+  actions[actions == "pass" & actions_extra == "Corner" & (match_events$pass.height.name == "High Pass" | match_events$pass.cross)] <- "corner_crossed"
+  actions[actions == "pass" & actions_extra == "Corner"] <- "corner_short"
+  actions[actions == "pass" & actions_extra == "Goal Kick"] <- "goalkick"
+  actions[actions == "pass" & actions_extra == "Throw-in"] <- "throw_in"
+  actions[actions == "pass" & match_events$pass.cross] <- "cross"
+  actions[actions == "duel" & actions_extra == "Tackle"] <- "tackle"
+  actions[actions == "duel"] <- "non_action"
+  actions[actions == "shot" & actions_extra == "Free Kick"] <- "shot_freekick"
+  actions[actions == "shot" & actions_extra == "Penalty"] <- "shot_penalty"
+  actions[actions == "goal keeper" & actions_extra == "Shot Saved"] <- "keeper_save"
+  actions[actions == "goal keeper" & actions_extra %in% c("Collected", "Keeper Sweeper")] <- "keeper_claim"
+  actions[actions == "goal keeper" & actions_extra == "Punch"] <- "keeper_punch"
+  actions[actions == "goal keeper"] <- "non_action"
+
+  #get the bodyparts uses for actions
+  bodyparts <- match_events[names(match_events)[grepl("body_part\\.name", names(match_events))]]
+  bodyparts <- dplyr::coalesce(!!!bodyparts)
+  bodyparts <- Rteta::spadl_dict("body", "statsbomb", bodyparts)
+
+  #get the result of each actions
+  results <- match_events[names(match_events)[
+    c(
+      grep("(?<!substitution)outcome.name", names(match_events), perl = TRUE),
+      grep("card.name", names(match_events)),
+      grep("clearance.body_part.name", names(match_events))
+    )]]
+  results <- dplyr::coalesce(!!!results)
+  results[which(match_events$type.name == "Own Goal Against")] <- "Own Goal Against"
+  results[which(match_events$type.name == "Miscontrol")] <- "Miscontrol"
+  results <- Rteta::spadl_dict("results", "statsbomb", results)
+
+  #bind everything together
+  spadl_df$action_id <- 1:nrow(spadl_df)
+  spadl_df$type_name = actions
+  spadl_df <- merge(spadl_df, spadl_type_ids, by = "type_name")
+  spadl_df <- spadl_df[order(spadl_df$action_id),]
+  spadl_df$bodypart_name = bodyparts
+  spadl_df$result_name = results
+  spadl_df <- cbind(spadl_df, match_locations)
+
+  #filter non actions
+  spadl_df <- spadl_df[-which(spadl_df$type_name == "non_action"),]
+  #fix direction of play
+  spadl_df$start_x[spadl_df$team_id != home_team] <- 105 - spadl_df$start_x[spadl_df$team_id != home_team]
+  spadl_df$end_x[spadl_df$team_id != home_team] <- 105 - spadl_df$end_x[spadl_df$team_id != home_team]
+  spadl_df$start_y[spadl_df$team_id != home_team] <- 68 - spadl_df$start_y[spadl_df$team_id != home_team]
+  spadl_df$end_y[spadl_df$team_id != home_team] <- 68 - spadl_df$end_y[spadl_df$team_id != home_team]
+  #fill in clearance end locations
+  spadl_df$end_x[spadl_df$type_name == "clearance"] <- spadl_df$start_x[which(spadl_df$type_name == "clearance") + 1]
+  spadl_df$end_y[spadl_df$type_name == "clearance"] <- spadl_df$start_y[which(spadl_df$type_name == "clearance") + 1]
+
+  #include extra dribbles
+  spadl_df <- dplyr::arrange(spadl_df, period_id, time_seconds)
+  extra_dribbles <- Rteta::split_dribbles(spadl_df)
+  spadl_df <- rbind(spadl_df, extra_dribbles)
+  spadl_df <- arrange(spadl_df, period_id, time_seconds)
+
+  return(spadl_df)
+}
+
+
 sb_convert_spadl <- function(match_events, save_dir = ".") {
   home_team <- match_events$team.id[1]
 
@@ -32,28 +131,6 @@ sb_convert_spadl <- function(match_events, save_dir = ".") {
   #rotate away team locations
 
 
-  actions <- match_events$type.name
-  actions_extra <- match_events %>%
-    dplyr::select(grep("\\.type\\.name", names(match_events)))
-  actions_extra <- dplyr::coalesce(!!!actions_extra)
-
-  actions <- Rteta::spadl_dict("action", "statsbomb", actions)
-
-  actions[actions == "pass" & actions_extra == "Free Kick" & (match_events$pass.height.name == "High Pass" | match_events$pass.cross)] <- "freekick_crossed"
-  actions[actions == "pass" & actions_extra == "Free Kick"] <- "freekick_short"
-  actions[actions == "pass" & actions_extra == "Corner" & (match_events$pass.height.name == "High Pass" | match_events$pass.cross)] <- "corner_crossed"
-  actions[actions == "pass" & actions_extra == "Corner"] <- "corner_short"
-  actions[actions == "pass" & actions_extra == "Goal Kick"] <- "goalkick"
-  actions[actions == "pass" & actions_extra == "Throw-in"] <- "throw_in"
-  actions[actions == "pass" & match_events$pass.cross] <- "cross"
-  actions[actions == "duel" & actions_extra == "Tackle"] <- "tackle"
-  actions[actions == "duel"] <- "non_action"
-  actions[actions == "shot" & actions_extra == "Free Kick"] <- "shot_freekick"
-  actions[actions == "shot" & actions_extra == "Penalty"] <- "shot_penalty"
-  actions[actions == "goal keeper" & actions_extra == "Shot Saved"] <- "keeper_save"
-  actions[actions == "goal keeper" & actions_extra %in% c("Collected", "Keeper Sweeper")] <- "keeper_claim"
-  actions[actions == "goal keeper" & actions_extra == "Punch"] <- "keeper_punch"
-  actions[actions == "goal keeper"] <- "non_action"
 
 
   #get the player minutes per match
@@ -75,6 +152,7 @@ sb_convert_spadl <- function(match_events, save_dir = ".") {
   results <- dplyr::coalesce(!!!results)
   results[which(match_events$type.name == "Own Goal Against")] <- "Own Goal Against"
   results[which(match_events$type.name == "Miscontrol")] <- "Miscontrol"
+  results <- Rteta::spadl_dict("results", "statsbomb", results)
 
   actions <- match_events %>%
     mutate(seconds = as.numeric(lubridate::hms(match_events$timestamp))) %>%
@@ -87,8 +165,8 @@ sb_convert_spadl <- function(match_events, save_dir = ".") {
     dplyr::filter(!is.na(player_id) & action != "non_action")
 
   extra_dribbles <- Rteta::split_dribbles(actions)
-  spadl <- rbind(actions, extra_dribbles) %>%
-    dplyr::arrange(period_id, time_seconds)
+  spadl_df <- rbind(spadl_df, extra_dribbles)
+  spadl_df <- dplyr::arrange(spadl_df, period_id, time_seconds)
 
   # if(!dir.exists(save_dir)) dir.create(save_dir)
   # if(!dir.exists(file.path(save_dir, "minutes"))) dir.create(file.path(save_dir, "minutes"))
@@ -194,12 +272,11 @@ split_dribbles <- function(spadl) {
   dribble_actions$end_x <- spadl$start_y[dribble_idx - 1]
   dribble_actions$end_x <- spadl$start_x[dribble_idx + 1]
   dribble_actions$end_y <- spadl$start_y[dribble_idx + 1]
-  dribble_actions$action <- "dribble"
+  dribble_actions$type_name <- "dribble"
+  dribble_actions$type_id <- 21
   dribble_actions$bodypart_name <- "Foot"
   dribble_actions$result_name <- "success"
   dribble_actions$time_seconds <- dribble_actions$time_seconds + 0.1
 
   return(dribble_actions)
 }
-
-
