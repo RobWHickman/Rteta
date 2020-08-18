6bdac2be990a657ab9ce75a493c9723b64096126
RobWHickman <robwhickman@gmail.com>
2020-08-18 14:27:55 +0100

    small changes

--- R/vaep_features.R
+++ R/vaep_features.R
@@ -1,5 +1,11 @@
-laggy_func <- function(vec, lags, name) {
+vaep_features_lag <- function(vec, lags, name) {
   x <- lapply(0:lags, lag, x = vec) %>% do.call(cbind, .) %>% as.data.frame()
   names(x) <- paste0(name, 0:lags)
   return(x)
+}
+
+vaep_get_features <- function(spadl) {
+  type_ids <- vaep_features_lag(spadl$type_id, 2, "type_id_a")
+  bodypart_ids <- vaep_features_lag(spadl$bodypart_id, 2, "bodypart_id_a")
+  result_ids <- vaep_features_lag(spadl$result_id, 2, "result_id_a")
 }
