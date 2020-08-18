ebe74bb8d62b343184dbd768f591c2d4e5207dc3
RobWHickman <robwhickman@gmail.com>
2020-08-18 10:46:17 +0100

    added full config files for ids

--- R/vaep_features.R
+++ R/vaep_features.R
@@ -1,10 +1,5 @@
-x <- lapply(0:n, lag, x = spadl_events$type_id) %>% do.call(cbind, .) %>% as.data.frame()
-names(x) <- paste0("type_id_a", seq(ncol(x)))
-
-
 laggy_func <- function(vec, lags, name) {
   x <- lapply(0:lags, lag, x = vec) %>% do.call(cbind, .) %>% as.data.frame()
   names(x) <- paste0(name, 0:lags)
   return(x)
 }
-FIX DUEL + LOST IN PLAY - SUCESS NOT FAILURE
