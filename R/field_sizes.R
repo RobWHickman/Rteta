a75457f68f708defea2a8ac308efcb14c56d7295
RobWHickman <robwhickman@gmail.com>
2020-09-06 18:37:45 +0100

    added event provider pitch dimensions

---
+++ R/field_sizes.R
@@ -0,0 +1,47 @@
+#' The width in the pitch specified for Statsbomb event data
+#'
+#' @docType data
+#'
+#' @usage data(statsbomb_width)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' width <- data(statsbomb_width)
+"statsbomb_width"
+
+#' The length in the pitch specified for Statsbomb event data
+#'
+#' @docType data
+#'
+#' @usage data(statsbomb_length)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' length <- data(statsbomb_length)
+"statsbomb_length"
+
+#' The width in the pitch specified for Opta event data
+#'
+#' @docType data
+#'
+#' @usage data(opta_width)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' width <- data(opta_width)
+"opta_width"
+
+#' The length in the pitch specified for Opta event data
+#'
+#' @docType data
+#'
+#' @usage data(opta_length)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' length <- data(opta_length)
+"opta_length"
